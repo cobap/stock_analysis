35edaf89e324e9fdea9bfbc2fec363c9e7d4f602
Antonio Coelho | Stone <antonio.coelho@stone.com.br>
2019-11-19 16:44:52 -0300

    Atualizando processamento dos números

--- process_stocks.py
+++ process_stocks.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import os, requests, re, time
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+# ML Libraries
+from sklearn.cluster import KMeans
 
 os.chdir('C:\\Users\\antonio.coelho\\codigos\\stock_analysis')
 
@@ -27,7 +31,6 @@ def stock_details(stock):
     detalhes['ultimo_balanco'] = table_rows[5].findAll('td')[3].span.text
     detalhes['enterprise_value'] = table_rows[6].findAll('td')[1].span.text
     detalhes['soma_total_acoes'] = table_rows[6].findAll('td')[3].span.text
-
 
     # Oscilações
     detalhes['variacao_dia'] = table_rows[8].findAll('td')[1].span.text
@@ -120,7 +123,47 @@ def _filtra_stocks(stock_list):
 
     return all_except_bdr
 
-if __name__ == '__main__':
+def recupera_stock_perdidos(nome_stocks_processados):
+
+    # nome_stocks_processados = 'detalhes_stock_20191111.xlsx'
+    stocks_processados = pd.read_excel(nome_stocks_processados)
+    stocks_processados['CODIGO']
+
+    stock_list = limpa_dataframe()
+    stock_list = stock_list[~stock_list['TIPO_ACAO'].isna()]
+    stock_list = _filtra_stocks(stock_list)
+
+    lista_processados = stocks_processados.apply(lambda row: row['CODIGO'][:-1:], axis=1)
+
+    nao_processados = stock_list[~stock_list['CODIGO'].isin(lista_processados)]
+    return nao_processados['CODIGO']
+
+def roda_pendencias(lista_nao_processados):
+
+    stocks_detailed = pd.DataFrame()
+
+    # Pegamos os detalhes para todos os códigos da B3
+    for _ticker in lista_nao_processados:
+
+        # Como não sabemos se é 3 ou 4, tentamos os 2 para todas as ações
+        _ticket_to_test = _ticker + '3'
+        _details = stock_details(_ticket_to_test)
+        _details['CODIGO'] = _ticket_to_test
+        stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
+        time.sleep(3)
+
+        _ticket_to_test = _ticker + '4'
+        _details = stock_details(_ticket_to_test)
+        _details['CODIGO'] = _ticket_to_test
+        stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
+        time.sleep(3)
+
+        print('-- Finalizado processamento para {0} --'.format(_ticker))
+
+    print('Finalizado processamento total')
+    return stocks_detailed
+
+def recupera_dados_stock(nome_arquivo):
 
     stock_list = limpa_dataframe()
 
@@ -135,30 +178,100 @@ if __name__ == '__main__':
 
     # Pegamos os detalhes para todos os códigos da B3
     for _ticker in stock_list['CODIGO']:
-        print(_ticker)
-        if _ticker in lista:
-            print(_ticker)
 
-            # Como não sabemos se é 3 ou 4, tentamos os 2 para todas as ações
-            _ticket_to_test = _ticker + '3'
-            _details = stock_details(_ticket_to_test)
-            _details['CODIGO'] = _ticket_to_test
-            stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
-            time.sleep(5)
+        # Como não sabemos se é 3 ou 4, tentamos os 2 para todas as ações
+        _ticket_to_test = _ticker + '3'
+        _details = stock_details(_ticket_to_test)
+        _details['CODIGO'] = _ticket_to_test
+        stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
+        time.sleep(3)
 
-            _ticket_to_test = _ticker + '4'
-            _details = stock_details(_ticket_to_test)
-            _details['CODIGO'] = _ticket_to_test
-            stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
-            time.sleep(5)
+        _ticket_to_test = _ticker + '4'
+        _details = stock_details(_ticket_to_test)
+        _details['CODIGO'] = _ticket_to_test
+        stocks_detailed = stocks_detailed.append(_details, ignore_index=True)
+        time.sleep(3)
 
-            print('-- Finalizado processamento para {0} --'.format(_ticker))
+        print('-- Finalizado processamento para {0} --'.format(_ticker))
 
+    print('Finalizado processamento total')
 
     stocks_detailed = stocks_detailed[~stocks_detailed['cotacao_dia'].isna()]
 
-# pendentes = stock_list[~stock_list['CODIGO'].isin(stocks_detailed['SOURCE'])]['CODIGO']
-# stocks_detailed['SOURCE'] = stocks_detailed.apply(lambda row: row['CODIGO'][:-1:], axis=1)
+    stocks_detailed.to_excel(nome_arquivo)
+
+    return stocks_detailed
+
+def _transform_stringcolumn_tonumber(nome_coluna):
+
+    nome_coluna = nome_coluna.str.replace('%', '')
+    nome_coluna = nome_coluna.str.replace('.', '@')
+    nome_coluna = nome_coluna.str.replace(',', '.')
+    nome_coluna = nome_coluna.str.replace('@', '')
+    nome_coluna = nome_coluna.apply(lambda row: 0 if len(row) == 1 else row)
+    nome_coluna = nome_coluna.astype(float)
+    return nome_coluna
+
+def _transform_comma_topoint(nome_coluna):
+
+    nome_coluna = nome_coluna.apply(lambda row: 0 if len(row) == 1 else row)
+    nome_coluna = nome_coluna.str.replace('.', '@')
+    nome_coluna = nome_coluna.str.replace(',', '.')
+    nome_coluna = nome_coluna.str.replace('@', '').astype(float)
+
+    return nome_coluna
+
+if __name__ == '__main__':
+
+    columns_name = [
+        'Index', 'CODIGO', 'cotacao_dia', 'crescimento_receita (%)', 'dividabruta_patrimonio', 'dividend_yeild (%)', 'ebit_ativo (%)', 'enterprise_value',
+        'ev_ebit', 'giro_ativos', 'liquidez_corr', 'lucro_por_acao', 'maior_cotacao_12_meses', 'margem_bruta', 'margem_ebit',
+        'margem_liquida', 'menor_cotacao_12_meses', 'p_ativo_circ_liq', 'p_ativos', 'p_capital_giro', 'p_ebit', 'preco_lucro',
+        'preco_valorpatrimonial', 'psr', 'roe', 'roic', 'soma_total_acoes', 'ultimo_balanco', 'ultimo_pregao', 'valor_mercado',
+        'variacao_12_meses', 'variacao_2014 (%)', 'variacao_2015 (%)', 'variacao_2016 (%)', 'variacao_2017 (%)', 'variacao_2018 (%)', 'variacao_2019 (%)',
+        'variacao_30_dias (%)', 'variacao_dia (%)', 'variacao_mes (%)', 'volume_medio_2_meses', 'vpa'
+    ]
+    dados_novembro = pd.read_excel('detalhes_stock_20191111.xlsx', names=columns_name)
+    dados_novembro = dados_novembro.iloc[:, 1:]
+
+    # Tranforma alguma colunas % em float
+    colunas_remover_porcentagem = ['crescimento_receita (%)', 'dividend_yeild (%)', 'ebit_ativo (%)', 'variacao_2014 (%)', 'variacao_2015 (%)', 'variacao_2016 (%)', 'variacao_2017 (%)', 'variacao_2018 (%)', 'variacao_2019 (%)', 'variacao_30_dias (%)', 'variacao_dia (%)', 'variacao_mes (%)', 'margem_bruta', 'margem_ebit', 'margem_liquida', 'roe', 'roic', 'variacao_12_meses']
+    for coluna in colunas_remover_porcentagem:
+        dados_novembro[coluna] = _transform_stringcolumn_tonumber(dados_novembro[coluna])
+
+    colunas_mudar_virgula_ponto = ['cotacao_dia', 'dividabruta_patrimonio', 'enterprise_value', 'ev_ebit', 'giro_ativos', 'liquidez_corr', 'lucro_por_acao', 'maior_cotacao_12_meses', 'menor_cotacao_12_meses', 'p_ativo_circ_liq', 'p_ativos', 'p_capital_giro', 'p_ebit', 'preco_lucro', 'preco_valorpatrimonial', 'psr', 'soma_total_acoes', 'valor_mercado', 'volume_medio_2_meses', 'vpa']
+    for coluna in colunas_mudar_virgula_ponto:
+        dados_novembro[coluna] = _transform_comma_topoint(dados_novembro[coluna])
+
+
+
+    """
+    EDA - Tratamento de dados
+    """
+    corr = dados_novembro.corr()
+    corr.style.background_gradient(cmap='coolwarm')
+    metricas_basicas = dados_novembro[['vpa', 'roic', 'roe', 'margem_ebit', 'lucro_por_acao', 'CODIGO']]
+
+    plt.scatter(metricas_basicas.iloc[:,0], metricas_basicas.iloc[:,1])
+    metricas_basicas[metricas_basicas.iloc[:,0] > 20000]
+
+
+
+    """
+    Tentando um ML básico para clusterização sem préprocessamento
+    """
+    knn_basico = dados_novembro[['vpa', 'roic', 'roe', 'margem_ebit', 'lucro_por_acao']]
+    kmeans = KMeans(n_clusters=4, random_state=0).fit(knn_basico)
+    kmeans.cluster_centers_
+    labels = kmeans.predict(knn_basico)
+
+
+
+
+
+
+
+
 
 # https://queroficarrico.com/blog/ferramentas-gratuitas-para-analise-fundamentalista/
 # https://www.fundamentus.com.br/detalhes.php?papel=PETR3
